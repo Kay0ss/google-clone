130b04e5a88a0cc8696e45648aa9e132876aeeb1
Jack <85977030+Kay0ss@users.noreply.github.com>
2021-12-27 14:19:53 -0600

    added hero image, searchbar, search button, and im feeling lucky button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
+import dots from "./Images/dots-menu.svg";
+import mic from "./Images/Google_mic.png";
+import hero from "./Images/google-logo.png";
+// import search from "./Images/search.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="landing-center">
+      <img className="hero" src={hero} alt="Google Logo"></img>
+      <div className="searchbar">
+        <img className="glass" src={mic} alt="search logo"></img>
+        <input className="search" name="search"></input>
+        <div className="mic-box">
+        <img className="mic" src={mic} alt="search logo"></img>
+        </div>
+      </div>
+      <div className="button-container">
+        <button>Google Search</button>
+        <button>I'm Feeling Lucky</button>
+      </div>
     </div>
   );
 }
