d6ac8c35a41a19f61401e8c5b11456fe5d6b33a1
Jack <85977030+Kay0ss@users.noreply.github.com>
2021-12-29 14:07:24 -0600

    added basic styling to page layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import dots from "./Images/dots-menu.svg";
 import mic from "./Images/Google_mic.png";
 import hero from "./Images/google-logo.png";
 // import search from "./Images/search.svg";
+import { AiOutlineSearch } from 'react-icons/ai';
 import "./App.css";
 
 function App() {
@@ -10,10 +11,12 @@ function App() {
     <div className="landing-center">
       <img className="hero" src={hero} alt="Google Logo"></img>
       <div className="searchbar">
-        <img className="glass" src={mic} alt="search logo"></img>
+          <div className="mic-box">
+            <h3 className="glass" ><AiOutlineSearch /></h3>
+          </div>
         <input className="search" name="search"></input>
         <div className="mic-box">
-        <img className="mic" src={mic} alt="search logo"></img>
+          <img className="mic" src={mic} alt="search logo"></img>
         </div>
       </div>
       <div className="button-container">
